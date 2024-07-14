c82f68c39ae3b741b69ff7d6f4200937df9e2d8e
Rahman <khasuev01@gmail.com>
2024-07-14 19:18:29 +0300

    r

--- PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
@@ -8,13 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         
         Cat beanCat =
                 (Cat) applicationContext.getBean("cat");
         Cat beanCat2 =
                 (Cat) applicationContext.getBean("cat");
         
-        System.out.println(bean.equals(beanCat));
+        System.out.println(bean.equals(bean1));
         System.out.println(beanCat.equals(beanCat2));
         
         
